9c0643d898bdf87fc0c08daf3b814a7bf90e5194
CarrloX <42298361+CarrloX@users.noreply.github.com>
2024-07-03 10:55:06 -0500

    Add files via upload

---
+++ ejercicio 3/src/HumanWorker.java
@@ -0,0 +1,11 @@
+// Clase para trabajador humano
+public class HumanWorker implements Workable, Eatable {
+    @Override
+    public void work() {
+    }
+
+    @Override
+    public void eat() {
+    }
+}
+
---
+++ ejercicio 3/src/RobotWorker.java
@@ -0,0 +1,6 @@
+// Clase para trabajador robot
+public class RobotWorker implements Workable {
+    @Override
+    public void work() {
+    }
+}
